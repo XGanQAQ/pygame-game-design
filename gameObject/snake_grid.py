dcfbca93ce429a55416364fd6437ea19f00ae145
GanX <2423855310@qq.com>
2025-06-13 21:14:03 +0800

    feat(snake): 蛇在受伤的时候会有变得透明的受伤动画

--- gameObject/snake_grid.py
+++ gameObject/snake_grid.py
@@ -28,6 +28,11 @@ class SnakeGrid(Gridmap):
 
         self.set_snake_art(tools.load_image("snake_head.png"), tools.load_image("snake_body.png"), tools.load_image("snake_tail.png"))
 
+        self.on_snake_get_hurt_animation_timer = 0
+        self.on_snake_get_hurt_animation_duration = 0.2 * 1000  # 动画持续时间: 默认 0.2秒，在后面会被初始化的和蛇的受伤间隔时间一致
+        self.is_snake_get_hurt_animation_end = False
+
+
     def update(self, keys, delta_time, plat_grid=None, item_grid=None, enemy_grid=None):
         if keys[pygame.K_LEFT]:
             self.direction = IntVector2(-1, 0)
@@ -40,6 +45,17 @@ class SnakeGrid(Gridmap):
 
         self.snake.update(delta_time, self.direction, self, plat_grid, item_grid, enemy_grid)
         self.__snake_map_grid(self.snake)
+
+        # 蛇受伤动画计时器
+        if self.on_snake_get_hurt_animation_timer:
+            self.on_snake_get_hurt_animation_timer -= delta_time * 1000  # delta_time 是秒，转换为毫秒
+            if self.on_snake_get_hurt_animation_timer <= 0:
+                self.on_snake_get_hurt_animation_timer = 0
+                self.is_snake_get_hurt_animation_end = True
+
+        if self.is_snake_get_hurt_animation_end:
+            self.on_snake_get_hurt_animation_end(self.snake)
+            self.is_snake_get_hurt_animation_end = False
 
     def draw(self, screen):
         """
@@ -64,11 +80,14 @@ class SnakeGrid(Gridmap):
                         screen.blit(self.snake_tail_image, (j * self.cell_size, i * self.cell_size))
                     else:
                         pygame.draw.rect(screen, (0, 150, 0), (j * self.cell_size, i * self.cell_size, self.cell_size, self.cell_size))
+        
     def roll(self):
         self.snake.roll()
 
     def init_snake(self, item_grid):
         self.snake = Snake(self.__grid_map_snake(), item_grid)
+        self.snake.snake_get_hurt_signal.connect(self.on_snake_get_hurt_animation_begin)
+        self.on_snake_get_hurt_animation_duration = self.snake.get_hurt_gap_time
 
     def set_snake_is_enable_falling(self, is_enable: bool):
         self.snake.is_enable_fall = is_enable
@@ -125,3 +144,25 @@ class SnakeGrid(Gridmap):
                 if self.get_cell(x, y) == 30 or self.get_cell(x, y) == 31 or self.get_cell(x, y) == 32:
                     snake_body.append(IntVector2(x, y))
         return snake_body
+    
+    def on_snake_get_hurt_animation_begin(self, sender, **kwargs):
+        """
+        蛇受伤动画
+        :param snake: 蛇对象
+        """
+        # 这里可以添加蛇受伤时的动画逻辑
+        self.snake_head_image.set_alpha(128)  # 例如将蛇头半透明显示
+        self.snake_body_image.set_alpha(128)  # 蛇身半透明显示
+        self.snake_tail_image.set_alpha(128)  # 蛇尾半透明显示
+        self.on_snake_get_hurt_animation_timer = self.on_snake_get_hurt_animation_duration
+
+    def on_snake_get_hurt_animation_end(self, sender, **kwargs):
+        """
+        蛇受伤动画结束
+        :param snake: 蛇对象
+        """
+        # 这里可以添加蛇受伤动画结束后的逻辑
+        self.snake_head_image.set_alpha(255)  # 恢复蛇头不透明
+        self.snake_body_image.set_alpha(255)
+        self.snake_tail_image.set_alpha(255)
+        self.on_snake_get_hurt_animation_timer = 0
